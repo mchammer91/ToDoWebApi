489573e972e79c58da12c79688e57d1d3c17c3f9
Michael Canova <michael.canova@q2ebanking.com>
2018-07-24 16:03:35 -0500

    need to add other CRUD methods

---
+++ ToDoApi/Controllers/ToDoController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoController(ToDoContext context)
+        {
+            _context = context;
+
+            if (_context.ToDoItems.Count() == 0)
+            {
+                _context.ToDoItems.Add(new ToDoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<ToDoItem>> GetAll()
+        {
+            return _context.ToDoItems.ToList();
+        }
+
+        [HttpGet("{id}", Name = "GetToDo")]
+        public ActionResult<ToDoItem> GetById(long id)
+        {
+            var item = _context.ToDoItems.Find(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return item;
+        }
+    }
+}
